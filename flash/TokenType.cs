084d5946475017f6b007902cf4b468ae6fdff0bf
Aman Shukla <shukla.aman303@gmail.com>
2018-12-08 19:07:00 +0530

    Added parenth token

--- flash/TokenType.cs
+++ flash/TokenType.cs
@@ -13,6 +13,7 @@ namespace Flash
         BadToken,
         EndOfFileToken,
         NumberExpression,
-        BinaryExpression
+        BinaryExpression,
+        ParenthesizedExpression
     }
 }
