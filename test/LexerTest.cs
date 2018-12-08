0ebccf1eb460c154deea5f7af605bd9e1e412a93
Aman Shukla <shukla.aman303@gmail.com>
2018-12-08 09:49:45 +0530

    updated test cases for lexer

--- test/LexerTest.cs
+++ test/LexerTest.cs
@@ -7,25 +7,83 @@ namespace Tests
     [TestFixture]
     public class LexerTest
     {
-        [Test]
-        public void Test()
-        {
-            //Arrange
-            var data = "1 + 2 * 3 / 4 ( )";
-            //Act 
-            var tokens = new Lexer(data).GetTokens().ToList();
-            //Assert
-            Assert.AreEqual(17, tokens.Count());
-            Assert.IsTrue(AreSame(new SyntaxToken(TokenKind.NumberToken,0,"1",1), tokens[0]));            
-
+        [TestCase("0",TokenKind.NumberToken)]
+        [TestCase("1",TokenKind.NumberToken)]
+        [TestCase("2",TokenKind.NumberToken)]
+        [TestCase("3",TokenKind.NumberToken)]
+        [TestCase("4",TokenKind.NumberToken)]
+        [TestCase("5",TokenKind.NumberToken)]
+        [TestCase("6",TokenKind.NumberToken)]
+        [TestCase("7",TokenKind.NumberToken)]
+        [TestCase("8",TokenKind.NumberToken)]
+        [TestCase("9",TokenKind.NumberToken)]
+        [TestCase("1234",TokenKind.NumberToken)]
+        [TestCase(" ",TokenKind.WhiteSpaceToken)]
+        [TestCase("  ",TokenKind.WhiteSpaceToken)]
+        [TestCase("+",TokenKind.PlusToken)]
+        [TestCase("-",TokenKind.MinusToken)]
+        [TestCase("/",TokenKind.SlashToken)]
+        [TestCase("*",TokenKind.StarToken)]
+        [TestCase("(",TokenKind.OpenParenthesisToken)]
+        [TestCase(")",TokenKind.CloseParenthesisToken)]
+        [TestCase("b",TokenKind.BadToken)]
+        [TestCase("",TokenKind.EndOfFileToken)]
+        public void LexerTokenKind_Test(string test, TokenKind expectedTokenKind)
+        {                       
+            Assert.AreEqual(expectedTokenKind, new Lexer(test).NextToken().Kind);
         }
 
-        private bool AreSame(SyntaxToken expected, SyntaxToken result)
-        {
-            return expected.Kind == result.Kind && 
-                    expected.Position == result.Position &&
-                    string.Equals(expected.Text,result.Text) &&
-                    expected.Value == expected.Value;
+        [TestCase("0", "0")]
+        [TestCase("1", "1")]
+        [TestCase("2", "2")]
+        [TestCase("3", "3")]
+        [TestCase("4", "4")]
+        [TestCase("5", "5")]
+        [TestCase("6", "6")]
+        [TestCase("7", "7")]
+        [TestCase("8", "8")]
+        [TestCase("9", "9")]
+        [TestCase("1234", "1234")]
+        [TestCase(" ", " ")]
+        [TestCase("  ", "  ")]
+        [TestCase("+", "+")]
+        [TestCase("+", "+")]
+        [TestCase("-", "-")]
+        [TestCase("/", "/")]
+        [TestCase("*", "*")]
+        [TestCase("(", "(")]
+        [TestCase(")", ")")]
+        [TestCase("", "\0")]
+        [TestCase("?", "?")]
+        public void LexerTokenText_Test(string test, string expectedText)
+        {                       
+            Assert.AreEqual(expectedText, new Lexer(test).NextToken().Text);
+        }
+
+        [TestCase("0", 0)]
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        [TestCase("4", 4)]
+        [TestCase("5", 5)]
+        [TestCase("6", 6)]
+        [TestCase("7", 7)]
+        [TestCase("8", 8)]
+        [TestCase("9", 9)]
+        [TestCase("1234", 1234)]
+        [TestCase(" ", null)]
+        [TestCase("  ", null)]
+        [TestCase("+", null)]
+        [TestCase("-", null)]
+        [TestCase("/", null)]
+        [TestCase("*", null)]
+        [TestCase("(", null)]
+        [TestCase(")", null)]
+        [TestCase("", null)]
+        [TestCase("?", null)]
+        public void LexerTokenValue_Test(string test, object expectedValue)
+        {                       
+            Assert.AreEqual(expectedValue, new Lexer(test).NextToken().Value);
         }
     }
 }
